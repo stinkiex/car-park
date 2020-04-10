5b942c6e3314363923ae8f396aa96f019722e520
Andrew <stinkiex@gmail.com>
2020-04-11 02:30:38 +0300

    add DefaultUserDao

--- web/src/main/java/com/github/stinkiex/carpark/web/WebUtil.java
+++ web/src/main/java/com/github/stinkiex/carpark/web/WebUtil.java
@@ -1,16 +1,12 @@
 package com.github.stinkiex.carpark.web;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
-
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class WebUtil {
-    private static final Logger log = LoggerFactory.getLogger(WebUtil.class);
+    //private static final Logger log = LoggerFactory.getLogger(WebUtil.class);
 
     public static void forword(String page, HttpServletRequest rq, HttpServletResponse rs) {
         try {
@@ -18,6 +14,13 @@ public class WebUtil {
         } catch (ServletException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    public static void redirect(String page, HttpServletRequest rq, HttpServletResponse rs) {
+        try {
+            rs.sendRedirect(rq.getContextPath() + page);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
