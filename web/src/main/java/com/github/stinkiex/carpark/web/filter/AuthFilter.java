18d081d40d4eaf42b9deb110c620ce74a6c69074
Andrew <stinkiex@gmail.com>
2020-04-14 09:59:50 +0300

    added AuthFilter

--- web/src/main/java/com/github/stinkiex/carpark/web/filter/AuthFilter.java
+++ web/src/main/java/com/github/stinkiex/carpark/web/filter/AuthFilter.java
@@ -1,10 +1,14 @@
 package com.github.stinkiex.carpark.web.filter;
 
+import com.github.stinkiex.carpark.web.WebUtil;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter
+@WebFilter("/cabinet") //todo Вкинуть ссылки
 public class AuthFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -13,7 +17,12 @@ public class AuthFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+        HttpServletRequest req = (HttpServletRequest)servletRequest;
+        Object authUser = req.getAttribute("authUser");
+        if (authUser==null){
+            WebUtil.redirect("index", req, (HttpServletResponse) servletResponse);
+        }
+        filterChain.doFilter(req, servletResponse);
     }
 
     @Override
